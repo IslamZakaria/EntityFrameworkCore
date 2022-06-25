c0aac080c78af047f56bb75812f46f22a94666b5
Islam Mohamed <eslam.mohamed@cis.asu.edu.eg>
2022-06-25 11:56:52 +0200

    added Posts Table

--- EntityFrameworkCore/Migrations/20220625095456_AddPostTable.Designer.cs
+++ EntityFrameworkCore/Migrations/20220625095456_AddPostTable.Designer.cs
@@ -4,14 +4,16 @@ using EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace EntityFrameworkCore.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20220625095456_AddPostTable")]
+    partial class AddPostTable
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
--- EntityFrameworkCore/Models/Post.cs
+++ EntityFrameworkCore/Models/Post.cs
@@ -6,11 +6,11 @@ using System.Threading.Tasks;
 
 namespace EntityFrameworkCore.Models
 {
-    public class Blog
+    public class Post
     {
         public int Id { get; set; }
-        public string Url { get; set; }
-
-        public List<Post> Posts { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public Blog Blog { get; set; }
     }
 }
